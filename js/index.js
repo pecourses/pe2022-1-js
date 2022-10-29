179d1d54584996e6114f85478b1c0ca6a6e116e7
Viktoriia <pecourses>
2022-10-29 16:40:03 +0300

    Add variables

---
+++ js/index.js
@@ -0,0 +1,34 @@
+// Виведення в консоль
+console.log('Hello, world!');
+
+// Вивести в модальне вікно
+// alert('Hello, world!');
+
+/*
+  Багатостроковий коментар
+*/
+
+// Змінні
+
+let a = 1;
+console.log('a =', a);
+a = a + 1;
+console.log('a =', a);
+
+// для уникнення випадкового переприсвоювання важливої інф-ї
+// рекомендується максимально вживати const
+const pi = 3.14; // не можна переприсвоювати значення!
+
+console.log(pi);
+
+var b = 10;
+console.log('b =', b);
+
+// Стиль іменування camelCase
+// lowerCamelCase
+// UpperCamelCase
+
+// PI, MATH_PI
+
+// 1    a-z, A-Z, _, $
+// 2... a-z, A-Z, _, $, 0-9
