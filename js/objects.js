66f182fc7eb3bdd4a767c6a997b70bd73903ec50
Viktoriia <pecourses>
2022-11-06 18:41:45 +0200

    Add objects

--- js/objects.js
+++ js/objects.js
@@ -1,67 +1,134 @@
-const firstName = 'Test';
-const lastName = 'Testovich';
-const email = 'test@gmail.com';
+// const firstName = 'Test';
+// const lastName = 'Testovich';
+// const email = 'test@gmail.com';
 
-// Оголошення обїєкту
-const obj1 = {}; // cинтаксичний цукор
-const obj2 = Object();
-const obj3 = new Object();
+// // Оголошення обїєкту
+// const obj1 = {}; // cинтаксичний цукор
+// const obj2 = Object();
+// const obj3 = new Object();
 
-// властивості: рядки, Symbol()
+// // властивості: рядки, Symbol()
+// const user = {
+//   // key: value,
+//   firstName: 'Test',
+//   lastName: 'Testovich',
+//   email: 'test@gmail.com',
+//   password: 'qwerty',
+// };
+
+// console.log('user :>> ', user);
+// // alert(user); // не вміє виводити об'єкти
+
+// // Доступ до властивостей (. - операція доступу до властивості)
+// // Читання властивості
+// console.log('user.email :>> ', user.email);
+// // Зміна значення властивостей
+// user.email = 'anothetmail@gmail.com';
+
+// // Додавання властивості
+// user.age = 10;
+// console.log('user :>> ', user);
+
+// // Видалення властивості
+// delete user.password;
+// console.log('user :>> ', user);
+
+// // Task: Створити об'єкт машини (марка, модель, рік випуску, номер, колір)
+// // Переглянути об'єкт
+// // змінити колір
+// // видалити властивість рік випуску
+// // додати властивість ім'я/прізвище власника
+// // Переглянути об'єкт
+// const car = {
+//   mark: 'VW',
+//   model: 'Jetta',
+//   year: 1987,
+//   number: '3642',
+//   color: 'Red',
+// };
+
+// console.log('car bebore :>> ', car);
+// car.color = 'Green';
+// delete car.year;
+// car.firstName = 'John';
+// car.lastName = 'Malcovich';
+// console.log('car after :>> ', car);
+
+// const capibara = {
+//   superPower: 'cute',
+//   countOfLegs: 3,
+//   countOfEyes: 2,
+//   favoriteThing: 'sun and water',
+// };
+
+// capibara.type = 'rat';
+// capibara.countOfLegs = 4;
+// delete capibara.favoriteThing;
+// console.log('capibara', capibara);
+
+// /* Типи-посилання (об'єкти) і типи-значення (примітиви) */
+// const a = 5;
+// const b = 5;
+// console.log('a===b :>> ', a === b);
+
+// const o1 = {};
+// const o2 = {};
+// console.log('o1===o2 :>> ', o1 === o2);
+
+// const o3 = o1;
+// console.log('o1===o3 :>> ', o1 === o3);
+// o3.name = 'property';
+// console.log('o1 :>> ', o1);
+
+// function f() {
+//   o3.lastName = 1;
+// }
+
+// f(o3);
+// console.log('o3 :>> ', o3);
+
+// /* Копіювання ЗНАЧЕННЯ об'єктів, (а не посилань) */
+// const user1 = Object.assign({}, user);
+// console.log('user1 :>> ', user1);
+// console.log('user===user1 :>> ', user === user1);
+
+// const user2 = { ...user };
+// console.log('user2 :>> ', user2);
+// console.log('user===user2 :>> ', user === user2);
+
+/*  */
+// const prop = prompt('input property');
+// const value = prompt('input value');
+
+// const obj = {
+//   [prop]: value,
+// };
+// console.log('obj :>> ', obj);
+
+/* */
+
 const user = {
-  // key: value,
+  // властивості
   firstName: 'Test',
   lastName: 'Testovich',
   email: 'test@gmail.com',
   password: 'qwerty',
+  // методи
+  // this - той, об'єкт, що стоїть перед крапкою при виклику метода
+  // getFullName: function () {
+  //   return `${this.firstName} ${this.lastName}`;
+  // },
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  // changeEmail: function (newEmail) {
+  //   this.email = newEmail;
+  // },
+  changeEmail(newEmail) {
+    this.email = newEmail;
+  },
 };
 
+console.log('user.getFullName() :>> ', user.getFullName());
+user.changeEmail('newmail@gmail.com');
 console.log('user :>> ', user);
-// alert(user); // не вміє виводити об'єкти
-
-// Доступ до властивостей (. - операція доступу до властивості)
-// Читання властивості
-console.log('user.email :>> ', user.email);
-// Зміна значення властивостей
-user.email = 'anothetmail@gmail.com';
-
-// Додавання властивості
-user.age = 10;
-console.log('user :>> ', user);
-
-// Видалення властивості
-delete user.password;
-console.log('user :>> ', user);
-
-// Task: Створити об'єкт машини (марка, модель, рік випуску, номер, колір)
-// Переглянути об'єкт
-// змінити колір
-// видалити властивість рік випуску
-// додати властивість ім'я/прізвище власника
-// Переглянути об'єкт
-const car = {
-  mark: 'VW',
-  model: 'Jetta',
-  year: 1987,
-  number: '3642',
-  color: 'Red',
-};
-
-console.log('car :>> ', car);
-car.color = 'Green';
-delete car.year;
-car.firstName = 'John';
-car.lastName = 'Malcovich';
-console.log('car :>> ', car);
-
-const capibara = {
-  superPower: 'cute',
-  countOfLegs: 3,
-  countOfEyes: 2,
-  favoriteThing: 'sun and water',
-};
-
-capibara.type = 'rat';
-capibara.countOfLegs = 4;
-delete capibara.favoriteThing;
-console.log('capibara', capibara);
