128537a47cb32e4480853f1dbab2aa2afce7ca58
Viktoriia <pecourses>
2022-11-06 16:53:59 +0200

    Add functions part2

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,10 @@
 // ФУНКЦІЇ
 
 // Повторне використання коду
+// Одна функція - одна відповідальність
 
 // Оголошення функції (function declaration)
+// Функцію можна викликати як до, так і після оголошення
 // function functionName(){
 //   function body
 // }
@@ -32,10 +34,19 @@ function sayHelloToUser(name) {
 sayHelloToUser(userName);
 
 // Повернення значень із функції
+// JSDoc
+/**
+ * Returns sum of two summands
+ * @param {number} a - First summand
+ * @param {number} b - Second summand
+ * @returns {number} Sum of two summands
+ */
 
 function sumAB(a, b) {
   return a + b;
 }
+
+sumAB(1, 5);
 
 const sum = sumAB(4, 5);
 console.log('sum :>> ', sum);
@@ -133,3 +144,87 @@ function getInitials(userFullName) {
 
 console.log('getInitials("") :>> ', getInitials('Ivo Smith'));
 console.log('getInitials("") :>> ', getInitials('Wally Brown'));
+
+// значення за умовчуванням / типове значення
+// Якщо значення на місце параметра за замовчуванням не передано,
+// то береться значення, вказане після = у списку параметрів
+// дефолтні значення мають бути перераховані ВКІНЦІ списку параметрів
+/**
+ * Power the base to the power of pow
+ * @param {number} base - base
+ * @param {number} [pow = 4] - power
+ * @returns {number} power the base to the power of pow
+ */
+function power(base, pow = 4) {
+  // if (pow === undefined) { - по факту, це працює так
+  //   pow = 4;
+  // }
+  return base ** pow;
+}
+
+console.log('power(10,2) :>> ', power(10));
+
+// Реалізувати функцію для розрахунку добутку чисел від
+// n1(=1) до n2
+// 10, 12 => 10*11*12
+// 3 => 1*2*3
+
+/**
+ * Returns multiplication between First Number and Last Number
+ * @param {number} n2 - Last Number
+ * @param {number} [n1 = 1] - First Number
+ * @returns {number} multiplication between First Number and Last Number
+ */
+function multiply(n2, n1 = 1) {
+  let power = 1;
+  for (n1; n1 <= n2; n1++) {
+    power *= n1;
+  }
+  return power;
+}
+console.log('multiply(4,2)', multiply(4));
+
+/* function expression / функциональное выражение / функціональний вираз */
+// не можна використовувати до ініціалізації
+const sum3 = function (a, b) {
+  return a + b;
+};
+
+console.log('sum3(2,5) :>> ', sum3(2, 5));
+
+// console.log('sum3(2,5) :>> ', sum3(2, 5));
+
+// Анонімна функція
+// function (a, b) {
+//   return a + b;
+// };
+
+// Переписати функцію для піднесення до степеня у формі function expression
+const pow = function (a, b) {
+  return a ** b;
+};
+
+console.log('pow(2,3)', pow(2, 3));
+
+// Чиста ф-я -
+// 1) детермінована ф-я - ф-я, яка при одних і
+// тих самих значеннях параметрів повертає один і той самий результат
+// 2) без побічних ефектів (мережеві запити, введення/виведення даних)
+
+// недетермінована ф-я
+function sum5(a) {
+  return a + Math.random();
+}
+console.log('sum5(1) :>> ', sum5(1));
+console.log('sum5(1) :>> ', sum5(1));
+
+// недетермінована ф-я
+function sum4(a) {
+  return a + b;
+}
+
+let b = 10;
+console.log('sum4(1) :>> ', sum4(1));
+
+b = 15;
+console.log('sum4(1) :>> ', sum4(1));
