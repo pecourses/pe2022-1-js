ade2d4462ab1fa6dbd7658d8ecf0b9ab162da77c
Viktoriia <pecourses>
2022-11-19 21:38:23 +0200

    Add classes

---
+++ js/classes.js
@@ -0,0 +1,79 @@
+function User1(name, surname, age, isMale, email, isSubscribed) {
+  this.firstName = name;
+  this.lastName = surname;
+  this.age = age;
+  this.isMale = isMale;
+  this.email = email;
+  this.isSubscribed = isSubscribed;
+}
+
+User1.prototype.getFullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+};
+// User1.isUser1();
+
+// Класи - синтаксичний цукор над вбудованими в жс функціями-конструкторами і прототипами
+class User {
+  constructor(name, surname, age, isMale, email, isSubscribed) {
+    this.firstName = name;
+    this.lastName = surname;
+    this.age = age;
+    this.isMale = isMale;
+    this.email = email;
+    this.isSubscribed = isSubscribed;
+  }
+  set age(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('age must be number');
+    }
+    if (value < 0 || value > 150 || !Number.isInteger(value)) {
+      throw new RangeError('age must be 0-130 integer');
+    }
+    this._age = value;
+  }
+  get age() {
+    return this._age;
+  }
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+  // get fullName() {
+  //   return `${this.firstName} ${this.lastName}`;
+  // }
+  static isUser(obj) {
+    return obj instanceof User;
+  }
+}
+
+// constructor - аналог функції-конструктора
+const user11 = new User('Test', 'Testovich', 25, true, 'test@gmail.com', true);
+
+try {
+  const user1 = new User('Test', 'Testovich', 25, true, 'test@gmail.com', true);
+  console.log('user1.getFullName() :>> ', user1.getFullName());
+  console.log('User.isUser({}) :>> ', User.isUser({}));
+  console.log('User.isUser(user1) :>> ', User.isUser(user1));
+  user1.age = 26;
+} catch (err) {
+  console.log('err :>> ', err);
+}
+
+// реалізувати клас для телефонів (марка, модель, колір, ціна, рік випуску)
+// реалізувати метод для розрахунку віку телефону
+// створити екземпляр класу, викликати для нього метод
+
+class Phone {
+  constructor(mark, model, color, price, year) {
+    this.mark = mark;
+    this.model = model;
+    this.color = color;
+    this.price = price;
+    this.year = year;
+  }
+  getAge() {
+    return new Date().getFullYear() - this.year;
+  }
+}
+
+const telephone1 = new Phone('Nokia', '3310', 'black', 2000, 2010);
+console.log(telephone1.getAge());
