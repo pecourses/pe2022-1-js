fed07258304165a2513ee0f9f1d6c162168d0537
Viktoriia <pecourses>
2022-11-20 17:46:12 +0200

    Add iterable

--- js/symbol.js
+++ js/symbol.js
@@ -28,3 +28,63 @@ switch (theme) {
   default:
     console.log(' unexpected value ');
 }
+
+// Symbol.iterator
+
+// 1 for..of
+
+// 2 деструктуризація колекцій
+const [first, second] = [1, 2];
+
+// 3 spread operator
+// ствонення копії колекції
+// конкатенація колекцій
+
+// для масивів:
+const arr1 = [1, 2, 3];
+
+const arr2 = [...arr1];
+
+function f() {
+  console.log('arguments :>> ', arguments);
+  const args = [...arguments];
+  console.log('args :>> ', args);
+}
+
+f(1, 2, 3);
+
+const arr3 = [9, 8, 7];
+
+const arr13 = [...arr1, ...arr3];
+console.log('arr13 :>> ', arr13);
+
+function f1(a, b, c) {
+  console.log('a,b,c :>> ', a, b, c);
+}
+
+const arr10 = [1, 2, 7];
+
+// f1(arr10[0], arr10[1], arr10[2]);
+f1(...arr10);
+
+// для об'єктів
+const user1 = { name: 'Test', age: 30 };
+const user2 = { ...user1 };
+
+const user3 = { isMale: true, isSubscribed: false };
+
+const user13 = { ...user1, ...user3 };
+
+sendRequest({ url: 'http://google.com' });
+
+function sendRequest(options) {
+  const defaultOptions = {
+    url: 'http://localhost',
+    port: 3000,
+  };
+  // отримати resultOptions з options і defaultOptions шляхом розширення (spread)
+  const resultOptions = { ...defaultOptions, ...options };
+  // resultOptions = { url: 'http://google.com', port: 3000, }
+}
+
+// 4 Array.from() - створює масив з псевдомасива або ітерованої колекції
